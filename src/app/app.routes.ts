e24ed9d0defdca963a41835954044bc3e3093c90
Ngqabutho Ncube <ngqabuthondlovu746@gmail.com>
2025-07-25 19:12:25 +0200

    Initial portfolio push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Career } from './career/career';
+import { About } from './about/about';
+import { Project } from './project/project';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  { path: 'career', component: Career},
+  { path: 'about', component: About },
+  { path: 'project', component: Project },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // Optional default
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
